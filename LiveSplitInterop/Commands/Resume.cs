29d13200e8e5249ec38bce3fbe42e1924223d2f3
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 16:50:46 -0500

    Add doc comments for `Resume`.

--- LiveSplitInterop/Commands/Resume.cs
+++ LiveSplitInterop/Commands/Resume.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that unpauses the timer if it's paused; otherwise, do nothing.
+    /// Games should use <see cref="UnpauseGameTime"/> instead.
+    /// </summary>
     public sealed class Resume : Command
     {
         public override string Message => "resume";
@@ -12,11 +16,22 @@ namespace LiveSplitInterop.Commands
 
     public static class ResumeExtensions
     {
+        /// <summary>
+        /// Unpause the timer if it's paused; otherwise, do nothing. Games should use
+        /// <see cref="UnpauseGameTimeExtensions.UnpauseGameTime(ILiveSplitCommandClient)"/>
+        /// instead.
+        /// </summary>
         public static void Resume(this ILiveSplitCommandClient client)
         {
             client.SendCommand(new Resume());
         }
 
+        /// <summary>
+        /// Unpause the timer asynchronously if it's paused; otherwise, do nothing. Games
+        /// should use
+        /// <see cref="UnpauseGameTimeExtensions.UnpauseGameTimeAsync(IAsyncLiveSplitCommandClient)"/>
+        /// instead.
+        /// </summary>
         public static async Task ResumeAsync(this IAsyncLiveSplitCommandClient client)
         {
             await client.SendCommandAsync(new Resume());
