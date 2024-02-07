91d8286b4c96c494835aafdc42addedc9bea385a
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 16:38:02 -0500

    Create PauseGameTime.cs

---
+++ LiveSplitInterop/Commands/PauseGameTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplitInterop.Commands
+{
+    public class PauseGameTime : Command
+    {
+        public override string Message => "pausegametime";
+    }
+
+    public static class PauseGameTimeExtensions
+    {
+        public static void PauseGameTime(this ILiveSplitCommandClient client)
+        {
+            client.SendCommand(new PauseGameTime());
+        }
+
+        public static async Task PauseGameTimeAsync(this IAsyncLiveSplitCommandClient client)
+        {
+            await client.SendCommandAsync(new PauseGameTime());
+        }
+    }
+}
