8ae030132d4ffbeddacf53ade4b317ccfd63a126
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 17:23:04 -0500

    Add doc comments for `Split`.

--- LiveSplitInterop/Commands/Split.cs
+++ LiveSplitInterop/Commands/Split.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that causes the timer to split.
+    /// </summary>
     public sealed class Split : Command
     {
         public override string Message => "split";
@@ -11,11 +14,17 @@ namespace LiveSplitInterop.Commands
 
     public static class SplitExtensions
     {
+        /// <summary>
+        /// Make the timer split.
+        /// </summary>
         public static void Split(this ILiveSplitCommandClient client)
         {
             client.SendCommand(new Split());
         }
 
+        /// <summary>
+        /// Make the timer split asynchronously.
+        /// </summary>
         public static async Task SplitAsync(this IAsyncLiveSplitCommandClient client)
         {
             await client.SendCommandAsync(new Split());
