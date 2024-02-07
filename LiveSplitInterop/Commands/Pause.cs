68f53686545387f8d6ae21b824657108015c6b7e
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 16:38:12 -0500

    Add docs to `Pause`.

--- LiveSplitInterop/Commands/Pause.cs
+++ LiveSplitInterop/Commands/Pause.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that pauses the timer. Games should use <see cref="PauseGameTime"/> instead.
+    /// </summary>
     public sealed class Pause : Command
     {
         public override string Message => "pause";
@@ -12,11 +15,23 @@ namespace LiveSplitInterop.Commands
 
     public static class PauseExtensions
     {
+        /// <summary>
+        /// Pause the timer. Games should use
+        /// <see cref="PauseGameTimeExtensions.PauseGameTime(ILiveSplitCommandClient)"/>
+        /// instead.
+        /// </summary>
         public static void Pause(this ILiveSplitCommandClient client)
         {
             client.SendCommand(new Pause());
         }
 
+        /// <summary>
+        /// Pause the timer asynchronously. Games should use
+        /// <see cref="PauseGameTimeExtensions.PauseGameTimeAsync(IAsyncLiveSplitCommandClient)"/>
+        /// instead.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         public static async Task PauseAsync(this IAsyncLiveSplitCommandClient client)
         {
             await client.SendCommandAsync(new Pause());
