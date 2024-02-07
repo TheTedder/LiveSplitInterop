9870c8223d61214e559cba29e7be4a20edd5d3fb
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 16:40:25 -0500

    Create UnpauseGameTime.cs

---
+++ LiveSplitInterop/Commands/UnpauseGameTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplitInterop.Commands
+{
+    public class UnpauseGameTime : Command
+    {
+        public override string Message => "unpausegametime";
+    }
+
+    public static class UnpauseGameTimeExtensions
+    {
+        public static void UnpauseGameTime(this ILiveSplitCommandClient client)
+        {
+            client.SendCommand(new UnpauseGameTime());
+        }
+
+        public static async Task UnpauseGameTimeAsync(this IAsyncLiveSplitCommandClient client)
+        {
+            await client.SendCommandAsync(new UnpauseGameTime());
+        }
+    }
+}
