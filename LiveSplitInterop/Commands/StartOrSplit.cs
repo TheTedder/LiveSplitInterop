68d4cefa99eb504dfedc8b21c1bc4c9ee29abaf0
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 17:29:12 -0500

    Add doc comments for `StartOrSplit`.

--- LiveSplitInterop/Commands/StartOrSplit.cs
+++ LiveSplitInterop/Commands/StartOrSplit.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that starts the timer if it's not running or causes it to split if it is.
+    /// </summary>
     public sealed class StartOrSplit : Command
     {
         public override string Message => "startorsplit";
@@ -11,11 +14,17 @@ namespace LiveSplitInterop.Commands
 
     public static class StartOrSplitExtensions
     {
+        /// <summary>
+        /// Start the timer if it's not running or make it split if it is.
+        /// </summary>
         public static void StartOrSplit(this ILiveSplitCommandClient client)
         {
             client.SendCommand(new StartOrSplit());
         }
 
+        /// <summary>
+        /// Asynchronously start the timer if it's not running or make it split if it is.
+        /// </summary>
         public static async Task StartOrSplitAsync(this IAsyncLiveSplitCommandClient client)
         {
             await client.SendCommandAsync(new StartOrSplit());
