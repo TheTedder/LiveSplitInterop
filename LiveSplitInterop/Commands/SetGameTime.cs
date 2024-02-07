e15b35795ed088855e05c9f8caa86fe399791891
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 17:11:46 -0500

    Add doc comments for `SetGameTime`.

--- LiveSplitInterop/Commands/SetGameTime.cs
+++ LiveSplitInterop/Commands/SetGameTime.cs
@@ -4,10 +4,19 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that sets the game time.
+    /// </summary>
     public sealed class SetGameTime : Command
     {
         private readonly TimeSpan gameTime;
 
+        /// <summary>
+        /// Constructs a new <see cref="SetGameTime"/>.
+        /// </summary>
+        /// <param name="gameTime">
+        /// The amount of time that has elapsed since the run started, not including loading times.
+        /// </param>
         public SetGameTime(TimeSpan gameTime)
         {
             this.gameTime = gameTime;
@@ -18,11 +27,17 @@ namespace LiveSplitInterop.Commands
 
     public static class SetGameTimeExtensions
     {
+        /// <summary>
+        /// Set the game time.
+        /// </summary>
         public static void SetGameTime(this ILiveSplitCommandClient client, TimeSpan gameTime)
         {
             client.SendCommand(new SetGameTime(gameTime));
         }
 
+        /// <summary>
+        /// Set the game time asynchronously.
+        /// </summary>
         public static async Task SetGameTimeAsync(this IAsyncLiveSplitCommandClient client, TimeSpan gameTime)
         {
             await client.SendCommandAsync(new SetGameTime(gameTime));
