59678df3141cb6c03f4c50499f0251de418bbe20
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 16:21:10 -0500

    Add doc comments for `GetCurrentRealtime`.

--- LiveSplitInterop/Commands/GetCurrentRealTime.cs
+++ LiveSplitInterop/Commands/GetCurrentRealTime.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that returns the current realtime.
+    /// </summary>
     public sealed class GetCurrentRealTime : Command<TimeSpan>
     {
         public override string Message => "getcurrentrealtime";
@@ -14,11 +17,17 @@ namespace LiveSplitInterop.Commands
 
     public static class GetCurrentRealTimeExtensions
     {
+        /// <summary>
+        /// Get the current realtime as a <see cref="TimeSpan"/>.
+        /// </summary>
         public static TimeSpan GetCurrentRealTime(this ILiveSplitCommandClient client)
         {
             return client.SendCommand(new GetCurrentRealTime());
         }
 
+        /// <summary>
+        /// Get the current realtime as a <see cref="TimeSpan"/> asynchronously.
+        /// </summary>
         public static async Task<TimeSpan> GetCurrentRealTimeAsync(this IAsyncLiveSplitCommandClient client)
         {
             return await client.SendCommandAsync(new GetCurrentRealTime());
