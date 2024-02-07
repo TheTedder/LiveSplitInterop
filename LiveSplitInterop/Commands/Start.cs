be3ba0b526d6a5450540a9d925dd09ec25483bfa
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 17:26:20 -0500

    Add doc comments for `Start`.

--- LiveSplitInterop/Commands/Start.cs
+++ LiveSplitInterop/Commands/Start.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that starts the timer.
+    /// </summary>
     public sealed class Start : Command
     {
         public override string Message => "start";
@@ -12,11 +15,17 @@ namespace LiveSplitInterop.Commands
 
     public static class StartExtensions
     {
+        /// <summary>
+        /// Start the timer.
+        /// </summary>
         public static void Start(this ILiveSplitCommandClient client)
         {
             client.SendCommand(new Start());
         }
 
+        /// <summary>
+        /// Start the timer.
+        /// </summary>
         public static async Task StartAsync(this IAsyncLiveSplitCommandClient client)
         {
             await client.SendCommandAsync(new Start());
