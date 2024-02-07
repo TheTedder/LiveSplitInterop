36a7e3e0b1953cb79835735336101ea721030618
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-07 17:19:05 -0500

    Add doc comments for `SkipSplit`.

--- LiveSplitInterop/Commands/SkipSplit.cs
+++ LiveSplitInterop/Commands/SkipSplit.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop.Commands
 {
+    /// <summary>
+    /// A command that skips the current split.
+    /// </summary>
     public sealed class SkipSplit : Command
     {
         public override string Message => "skipsplit";
@@ -12,11 +15,17 @@ namespace LiveSplitInterop.Commands
 
     public static class SkipSplitExtensions
     {
+        /// <summary>
+        /// Skip the current split.
+        /// </summary>
         public static void SkipSplit(this ILiveSplitCommandClient client)
         {
             client.SendCommand(new SkipSplit());
         }
 
+        /// <summary>
+        /// Skip the current split asynchronously.
+        /// </summary>
         public static async Task SkipSplitAsync(this IAsyncLiveSplitCommandClient client)
         {
             await client.SendCommandAsync(new SkipSplit());
