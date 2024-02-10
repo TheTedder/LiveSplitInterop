a1f49ebbcba61e4516bbfdab4c03bf8c611c9568
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-02-10 13:07:41 -0500

    Create AlwaysPauseGameTime.cs

---
+++ LiveSplitInterop/Commands/AlwaysPauseGameTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplitInterop.Commands
+{
+    /// <summary>
+    /// A command that stops game time from automatically advancing.
+    /// </summary>
+    public class AlwaysPauseGameTime : Command
+    {
+        public override string Message => "alwayspausegametime";
+    }
+
+    public static class AlwaysPauseGameTimeExtensions
+    {
+        /// <summary>
+        /// Stop game time from automatically advancing.
+        /// </summary>
+        public static void AlwaysPauseGameTime(this ILiveSplitCommandClient client)
+        {
+            client.SendCommand(new AlwaysPauseGameTime());
+        }
+
+        /// <inheritdoc cref="AlwaysPauseGameTime(ILiveSplitCommandClient)"/>
+        public static async Task AlwaysPauseGameTimeAsync(this IAsyncLiveSplitCommandClient client)
+        {
+            await client.SendCommandAsync(new AlwaysPauseGameTime());
+        }
+    }
+}
