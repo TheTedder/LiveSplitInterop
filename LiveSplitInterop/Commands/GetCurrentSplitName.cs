f19d3d0408fc12366ad990fb5bec592db8bd7346
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-04-12 13:44:34 -0400

    More commands (#2)
    * Create AlwaysPauseGameTime.cs
    
    * Add missing `sealed` keyword.
    
    * Add getcurrentsplitname command.
    
    * Add GetDelta command.
    
    * Update UndoSplit.cs

---
+++ LiveSplitInterop/Commands/GetCurrentSplitName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplitInterop.Commands
+{
+    /// <summary>
+    /// A command that returns the current split name. Returns <c>-</c> if the timer state is ended or not running.
+    /// </summary>
+    public sealed class GetCurrentSplitName : Command<string>
+    {
+        public override string Message => "getcurrentsplitname";
+
+        public override string ParseResponse(string response) => response;
+    }
+
+    public static class GetCurrentSplitNameExtensions
+    {
+        public static string GetCurrentSplitName(this ILiveSplitCommandClient client)
+        {
+            return client.SendCommand(new GetCurrentSplitName());
+        }
+
+        public static async Task<string> GetCurrentSplitNameAsync(this IAsyncLiveSplitCommandClient client)
+        {
+            return await client.SendCommandAsync(new GetCurrentSplitName());
+        }
+    }
+}
---
+++ LiveSplitInterop/Commands/GetDelta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplitInterop.Commands
+{
+    /// <summary>
+    /// A command that returns the delta for the last split against the current comparison.
+    /// </summary>
+    public sealed class GetDelta : Command<TimeSpan?>
+    {
+        public override string Message => "getdelta";
+
+        public override TimeSpan? ParseResponse(string response)
+        {
+            return Util.ParseLiveSplitTimeSpanNullable(response);
+        }
+    }
+
+    public static class GetDeltaExtensions
+    {
+        public static TimeSpan? GetDelta(this ILiveSplitCommandClient client)
+        {
+            return client.SendCommand(new GetDelta());
+        }
+
+        public static async Task<TimeSpan?> GetDeltaAsync(this IAsyncLiveSplitCommandClient client)
+        {
+            return await client.SendCommandAsync(new GetDelta());
+        }
+    }
+}
