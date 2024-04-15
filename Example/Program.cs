e825c84b1f3da11dee13b5f344963aaeff669f78
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-04-15 18:13:39 -0400

    Tcp client (#4)
    * Add debug commands.
    
    * Create TcpCommandClient.cs
    
    * Add more cmds to example program.

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,17 +1,22 @@
 ﻿using LiveSplitInterop.Clients;
 using LiveSplitInterop.Commands;
+using System.Net;
 
 using NamedPipeCommandClient client = new();
+//using TcpCommandClient client = new("localhost", 16834);
 Console.WriteLine("Connecting to LiveSplit...");
 await client.ConnectAsync();
 Console.WriteLine(
 @"Connected! Available commands: 
-[Q]     Quit
-[Enter] Start or split
-[I]     Print current split index
-[SG]    Set game time
-[GR]    Get current real time
-[U]     Undo split");
+[Q]      Quit
+[Enter]  Start or split
+[I]      Print current split index
+[SG]     Set game time
+[GR]     Get current real time
+[U]      Undo split
+[D]      Get delta
+[N]      Get current split name
+[Insert] Custom command");
 
 bool quit = false;
 while (!quit)
@@ -21,47 +26,103 @@ while (!quit)
     switch (key.Key)
     {
         case ConsoleKey.Q:
+            {
             quit = true;
+
             break;
+            }
 
         case ConsoleKey.Enter:
+            {
+
             await client.StartOrSplitAsync();
             break;
+            }
 
         case ConsoleKey.I:
+            {
+
             int index = await client.GetSplitIndexAsync();
             Console.WriteLine(index);
             break;
+            }
 
         case ConsoleKey.S:
-            switch (Console.ReadKey(true).Key)
             {
-                case ConsoleKey.G:
-                Console.Write("Set game time to (Enter a time): ");
-                string? line = Console.ReadLine();
-
-                if (line != null)
+                switch (Console.ReadKey(true).Key)
                 {
-                    await client.SetGameTimeAsync(TimeSpan.Parse(line));
+                    case ConsoleKey.G:
+                        Console.Write("Set game time to (Enter a time): ");
+                        string? line = Console.ReadLine();
+
+                        if (line != null)
+                        {
+                            await client.SetGameTimeAsync(TimeSpan.Parse(line));
+                        }
+
+                        break;
                 }
 
                 break;
             }
 
-            break;
-            
         case ConsoleKey.G:
-            switch (Console.ReadKey(true).Key)
             {
-                case ConsoleKey.R:
-                    Console.WriteLine("Current time: {0:c}", await client.GetCurrentRealTimeAsync());
-                    break;
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.R:
+                        Console.WriteLine("Current time: {0:c}", await client.GetCurrentRealTimeAsync());
+                        break;
+                }
+
+                break;
             }
 
-            break;
-
         case ConsoleKey.U:
-            await client.UndoSplitAsync();
-            break;
+            {
+                await client.UndoSplitAsync();
+                break;
+            }
+
+        case ConsoleKey.D:
+            {
+                TimeSpan? delta = await client.GetDeltaAsync();
+                Console.WriteLine("Delta: {0:c}", delta?.ToString("c") ?? "-");
+                break;
+            }
+
+        case ConsoleKey.N:
+            {
+                string splitname = await client.GetCurrentSplitNameAsync();
+                Console.WriteLine("Current Split: {0}", splitname);
+                break;
+            }
+
+#if DEBUG
+        case ConsoleKey.Insert:
+            {
+                Console.Write('>');
+
+                string? line = Console.ReadLine();
+
+                if (line is not null)
+                {
+                    await client.SendCommandRaw(line);
+
+                    if (true || (key.Modifiers & ConsoleModifiers.Shift) != 0)
+                    {
+                        string? res = await client.ConsumeLineAsync();
+                    
+                        if (res is not null)
+                        {
+                            //Console.WriteLine("{0:x}", (short)res[0]);
+                            Console.WriteLine(res);
+                        }
+                    }
+                }
+
+                break;
+            }
+#endif
     }
 }
