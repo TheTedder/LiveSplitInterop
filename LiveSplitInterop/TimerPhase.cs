c039c249db13dab389b2b41ce805135e22c146cf
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-04-28 18:27:25 -0400

    Create TimerPhase.cs

---
+++ LiveSplitInterop/TimerPhase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveSplitInterop
+{
+    public enum TimerPhase
+    {
+        NotRunning,
+        Running,
+        Ended,
+        Paused
+    }
+}
