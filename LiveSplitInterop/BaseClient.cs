d58f617357e0fea4faf4861bdabb1fc47a9a42c4
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-05-24 16:23:59 -0400

    Add docs to BaseClient.

--- LiveSplitInterop/BaseClient.cs
+++ LiveSplitInterop/BaseClient.cs
@@ -6,17 +6,43 @@ using System.Threading.Tasks;
 
 namespace LiveSplitInterop
 {
+    /// <summary>
+    /// Abstract base class for clients implementing <see cref="ILiveSplitCommandClient"/> and <see cref="IAsyncLiveSplitCommandClient"/>.
+    /// </summary>
     public abstract class BaseClient : IDisposable, ILiveSplitCommandClient, IAsyncLiveSplitCommandClient
     {
+        /// <summary>
+        /// The <see cref="StreamReader"/> used to read data from LiveSplit.
+        /// </summary>
+        /// <remarks>
+        /// Ensure that <see cref="Setup(Stream)"/> has been called once before use.
+        /// </remarks>
         protected StreamReader Reader { get; set; }
+
+        /// <summary>
+        /// The <see cref="StreamWriter"/> used to send data to LiveSplit.
+        /// </summary>
+        /// <remarks>
+        /// Ensure that <see cref="Setup(Stream)"/> has been called once before use.
+        /// </remarks>
         protected StreamWriter Writer { get; set; }
+
+        /// <summary>
+        /// The underlying <see cref="Stream"/> representing the flow of data to and from LiveSplit.
+        /// </summary>
+        /// <remarks>
+        /// Ensure that <see cref="Setup(Stream)"/> has been called once before use.
+        /// </remarks>
         protected Stream Stream;
+
+        /// <summary>
+        /// A <see cref="bool"/> representing whether the client is connected to LiveSplit.
+        /// </summary>
         public abstract bool IsConnected { get; }
 
         /// <summary>
         /// Sets up or reinitializes the <see cref="Reader"/> and <see cref="Writer"/>
-        /// for use with <paramref name="stream"/>. This method should be called before
-        /// attempting to send any commands.
+        /// for use with <paramref name="stream"/>.
         /// </summary>
         /// <remarks>
         /// This method should be called before attempting to send any commands.
